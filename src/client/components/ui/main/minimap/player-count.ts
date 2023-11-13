9886731400c418275258ea42bc1b08af6f70d4a9
AlphaRunic <roleyspersonal@gmail.com>
2023-11-12 23:54:48 -0500

    feat: create PlayerCount component for main UI

---
+++ src/client/components/ui/main/minimap/player-count.ts
@@ -0,0 +1,17 @@
+import type { OnStart } from "@flamework/core";
+import { Component, BaseComponent } from "@flamework/components";
+import { Players } from "@rbxts/services";
+
+import { Player } from "shared/utilities/client";
+
+@Component({
+  tag: "MainUI_PlayerCount",
+  ancestorWhitelist: [ Player.WaitForChild("PlayerGui") ]
+})
+export class PlayerCount extends BaseComponent<{}, PlayerGui["Main"]["Minimap"]["GameInfo"]["PlayerCount"]> implements OnStart {
+  public onStart(): void {
+    const update = () => this.instance.Value.Text = tostring(Players.GetPlayers().size());
+    Players.PlayerAdded.Connect(update);
+    Players.PlayerRemoving.Connect(update);
+  }
+}
